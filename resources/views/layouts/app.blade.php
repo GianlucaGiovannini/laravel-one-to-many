01b93e6bad11fcf5e84aa131e78e1d4deaa03267
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-06-23 16:12:05 +0200

    review

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
 <body>
 
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
@@ -90,7 +90,7 @@
     </nav>
 
     @yield('app')
-    
+
 </body>
 
 </html>
