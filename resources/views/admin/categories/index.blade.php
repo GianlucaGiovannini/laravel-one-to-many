ddb542c61d66be6b3a2cdbeff52863e6b7947358
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-06-23 16:39:32 +0200

    review

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -10,11 +10,11 @@
     @include('partials.errors')
 
     <div class="row ">
-        <div class="col pt-4 pe-5">
-            <form action="" method="post" class="d-flex align-items-center">
+        <div class="col pe-5 pt-5">
+            <form action="" method="post" class="d-flex align-items-center mt-5">
                 @csrf
                 <div class="input-group mb-3">
-                    <input type="text" name="name" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <input type="text" name="name" class="form-control" placeholder="Nome nuova categoria" aria-label="Nome nuova categoria" aria-describedby="button-addon2">
                     <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Add</button>
                 </div>
             </form>
@@ -54,7 +54,7 @@
                         </td>
 
                         <td>
-                            <button form="category-{{$category->id}}" type="submit" class="btn btn-primary mb-1 text-white">
+                            <button form="category-{{$category->id}}" type="submit" class="btn btn-primary m-1 text-white">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z" />
                                     <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z" />
@@ -64,7 +64,7 @@
 
 
                             <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-danger text-white" data-bs-toggle="modal" data-bs-target="#delete-category-{{$category->id}}">
+                            <button type="button" class="btn btn-danger m-1 text-white" data-bs-toggle="modal" data-bs-target="#delete-category-{{$category->id}}">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                     <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
